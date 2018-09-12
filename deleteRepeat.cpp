56ed9cab00251fa42ff5f143e7b02c6d713bbba5
Patrick M. Howard <pmh41@zips.uakron.edu>
2018-09-12 12:40:33 -0400

    Modified deleteRepeat.cpp, I'll try to modify the actual algorithm...

--- deleteRepeat.cpp
+++ deleteRepeat.cpp
@@ -7,9 +7,8 @@
 
 */
 #include<iostream>
-#include<memory> //For unique pointers
-
-std::unique_ptr<char[]> deleteRepeat(char[]);
+#include<memory> //For unique pointers 
+char* deleteRepeat(char[]);
 const int SIZE = 10;
 
 int main()
@@ -34,39 +33,18 @@ int main()
   }
   std::cout << std::endl << std::endl;
 
-  std::unique_ptr<char[]> noRepeats = deleteRepeat(originalArray);
+  char* noRepeats = deleteRepeat(originalArray);
 
   return 0;
 }
 
-std::unique_ptr<char[]> deleteRepeat(char originalLetters[] )
+char* deleteRepeat(char originalLetters[] )
 {
   //Create a new dynamic array, using constant SIZE to determine initial size.
-  std::unique_ptr<char[]> uniqueChars;
-  uniqueChars = new  char[SIZE];
+  char* uniqueChars;
+  uniqueChars = new char[SIZE];
   
-  //Append the first original letter to the dyanmic array.
-  int nUniqueChars = 1;
-  uniqueChars[0] = originalLetters[0];
-  
-  //The variable nUniqueChars defines the number and the last position in the dynamic array.
-
-  for(int i = 0; i <= nUniqueChars; ++i) //For every unique letter encountered...
-  {
-   for(int j = 0; j < SIZE; ++j) //For every element in the array passed into the function.
-   {
-     if(originalLetters[j] != uniqueChars[i])
-     {
-       //Append and increment the value of nUniqueChars
-       uniqueChars[nUniqueChars] = originalLetters[j];
-       nUniqueChars++;
-     }
-     else
-     {
-       //Movo.
-     }
-   } 
-  }
+   
   
   return uniqueChars;
  
