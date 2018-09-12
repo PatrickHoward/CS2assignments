a2f3a44359884767e5259705274f8cdd02f8733e
Patrick M. Howard <pmh41@zips.uakron.edu>
2018-09-12 17:13:17 -0400

    Finished deleteRepeat.cpp

--- deleteRepeat.cpp
+++ deleteRepeat.cpp
@@ -1,19 +1,28 @@
 /*
   Computer Science II - Assignment A - Patrick M. Howard <pmh41@zips.uakron.edu>
   The University of Akron
-
+  
+  Desc: This program takes an array of characters and returns another array of characters
+  	that are not repeated in the input.   
+    
   Assignment due date - 9/12/2018
   Copyright 2018, all rights reserved
 
 */
 #include<iostream>
 #include<memory> //For unique pointers 
-char* deleteRepeat(char[]);
+
+std::unique_ptr<char[]> deleteRepeat(char[], int&);
+//Pre:  An array of characters is taken in, and a empty integer is passed in by reference 
+//      (so that the number of unique characters can be printed in the main function.
+//Post: An array of unique characters is returned via a std::unique_ptr<char[]>.
+
 const int SIZE = 10;
 
 int main()
 {
   char originalArray[SIZE];
+
   originalArray [0] = 'a';
   originalArray [1] = 'b';
   originalArray [2] = 'b';
@@ -25,27 +34,64 @@ int main()
   originalArray [8] = 'b';
   originalArray [9] = 'c';
 
+  int numOfUniques;
+
   //Print the original array...
-  std::cout << "Here is the test case: " << std::endl;
-  for(int i = 0; i < SIZE; i++)
+  std::cout << "Here is the test case: \n";
+  for(int i = 0; i < SIZE-1; i++)
   {
     std::cout << originalArray[i] << ", "; //Print each element separated by a comma and a space.
   }
-  std::cout << std::endl << std::endl;
-
-  char* noRepeats = deleteRepeat(originalArray);
+  std::cout << originalArray[SIZE-1] << '\n';
+  
+  std::unique_ptr<char[]> noRepeats = deleteRepeat(originalArray, numOfUniques );
+  
+  std::cout << "And here are the unique characters: \n";
+  for(int i = 0; i < numOfUniques-1; i++)
+  {
+    std::cout << noRepeats[i] << ", ";
+  
+  }
+  std::cout << noRepeats[numOfUniques-1];
+  std::cout << "\n There are " << numOfUniques << " in the given sequence of chars\n";
 
   return 0;
 }
 
-char* deleteRepeat(char originalLetters[] )
+std::unique_ptr<char[]> deleteRepeat(char originalLetters[], int& arrSize)
 {
-  //Create a new dynamic array, using constant SIZE to determine initial size.
-  char* uniqueChars;
-  uniqueChars = new char[SIZE];
-  
+  int nUniqueChars = 0; //Number of unique characters that exists in originalLetters[]
+  char highestChar ='-'; //The highest character that exists alphabetically.
+
+  //Run the first loop to find the quantity of unique characters.
+  for(int i = 0; i < SIZE; i++)
+  {
+    if(originalLetters[i] > highestChar )//If the character comes after the highest character
+    { 
+      nUniqueChars++;
+      highestChar=originalLetters[i];
+    }
+  }
+
+  std::unique_ptr<char[]> uniqueChars(new char[nUniqueChars]);//Creates a new dynamically allocated array that is the size of nUniqueChars 
+
+  highestChar = '-'; //Reset the value of highestChar. 
+
+  //Run the second loop to find the first occurance each unique character.
+  for(int i = 0; i < nUniqueChars; i++)
+  {
+    for(int j = 0; j < SIZE; j++)
+    {
+      if(originalLetters[j] > highestChar)
+      {
+        highestChar = originalLetters[j];
+	uniqueChars[i] = originalLetters[j];
+	break;
+      }
+    }
+  }   
    
-  
+  arrSize = nUniqueChars; 
   return uniqueChars;
  
 
