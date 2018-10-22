a00e413941cce053fd9397bca38360581d4c693a
Patrick M. Howard <pmh41@zips.uakron.edu>
2018-10-21 23:56:03 -0400

    Fuck this project, they are limiting me to 5 files to handle everything...

--- projectPartB/projectPartB.cpp
+++ projectPartB/projectPartB.cpp
@@ -8,7 +8,7 @@
 */
 
 #include <string>
-#include "Menu.hpp"
+#include <iostream>
 
 //Global Constants
 //Number of computer Labs
@@ -19,7 +19,56 @@ const int LABSIZES[NUMLABS] = {19, 15, 24, 33, 61, 17, 55, 37};
 //Universities currently under constract.
 const std::string UNIVERSITYNAMES[NUMLABS] = {"The University of Michigan", "The University of Pittsburgh", "Stanford University", "Arizona State University", "North Texas State University", "The University of Alabama, Huntsville", "Princeton University", "Duquesne University"};
 
+struct Menu
+{
+    void printHeader();
+ 
+    void printLabs();
+ 
+    void printMenu();
+
+    void printStartup();
+};
+
 int main()
 {
+  Menu instance; 
+  instance.printStartup();  
+
+  
 
 } 
+
+void Menu::printHeader()
+{
+  std::cout << "|-- LabTrac 2 - Student Computer Lab Tracking System --|\n"
+       << "|-- Created by Patrick M. Howard - For education use --|\n";
+
+}
+
+void Menu::printLabs()
+{
+  for(int i = 0; i < NUMLABS; ++i)
+  {
+    std::cout << "| Lab #" << i+1 << " for " << UNIVERSITYNAMES[i] << "\n";
+  }
+}
+
+void Menu::printMenu()
+{
+  std::string menuOptions[5] = {"Simulate Login", "Simulate Logoff", "Search", "Display Lab", "Quit"};
+  
+  std::cout << "| MAIN MENU\n";
+  for(int i = 0; i < 5; ++i)
+  {
+    std::cout << "| " << i+1 << ") " << menuOptions[i] << "\n";
+  }
+  
+}
+
+void Menu::printStartup()
+{
+  printHeader();
+  printLabs();
+  printMenu();
+}
